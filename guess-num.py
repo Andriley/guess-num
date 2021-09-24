194e9c08f6b3de1f4f18765d291a3a5762f4733e
Andy Lam <andylammm42@gmail.com>
2021-09-24 11:00:39 +0100

    add count

--- guess-num.py
+++ guess-num.py
@@ -1,14 +1,19 @@
 import random
 
 r = random.randint(1,100)
+t = 0
 while True:
 	num = input('please guess the num: ')
 	num = int(num)
 	if num == r:
 		print('correct answer!')
+		t = t + 1
 		break
 	else:
 		if num > r:
 			print(num,'is bigger than the answer')
+			t = t + 1
 		else:
-			print(num, 'is smaller than the answer')
+		 	print(num, 'is smaller than the answer')
+		 	t = t + 1
+print('you have tried', t, 'times')
